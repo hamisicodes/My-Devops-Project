4299877747d2af502d452d047c3f5f4d342858ab
hamisicodes <newhamisi@gmail.com>
2021-07-24 20:58:22 +0300

    Initial commit for python app

---
+++ app/flaskr/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, Devops!'
+
+    return app
